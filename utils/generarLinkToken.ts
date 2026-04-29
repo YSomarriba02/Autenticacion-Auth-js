142ec580971a901c115390edc5aef1cf34b8dc3a
Somarriba hi <128437648+somarriba02polz@users.noreply.github.com>
2026-04-28 20:02:50 -0600

    fix: link de generarLinToken

--- utils/generarLinkToken.ts
+++ utils/generarLinkToken.ts
@@ -1,5 +1,5 @@
 export default function generarLinkToken({ token, id_usuario }: { token: string, id_usuario: number }) {
-    const base = "http://localhost:3000/api/validaremail/"
+    const base = "http://autenticacion-auth-js.onrender.com/api/validaremail/"
     const url = new URL(base)
     url.searchParams.append("id", String(id_usuario))
     url.searchParams.append("token", token)
